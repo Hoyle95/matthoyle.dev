149e9ffc9ef741ab2d9c1bd9c5ec69ec40895350
Hoyle95 <140357015+Hoyle95@users.noreply.github.com>
2025-04-14 17:58:43 +0100

    templates

--- js/validate.js
+++ js/validate.js
@@ -1,7 +1,7 @@
 function validate() {
-    if (document.querySelector("#message").value != "") {
+    if (Boolean(document.querySelector("#message").value)) {
         let form = document.querySelector("form");
         form.submit();
         form.innerHTML = "<h1 style='text-align:center;'>Sending...</h1>";
-    }
+    }ß
 }
