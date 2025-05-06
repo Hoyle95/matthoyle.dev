1c1f726493ca3b3ee26d72918759112131c7c848
Hoyle95 <140357015+Hoyle95@users.noreply.github.com>
2025-05-06 06:48:20 +0100

    Neil Buchanan quotes

--- P5-Sandbox/art_attack/js/main.js
+++ P5-Sandbox/art_attack/js/main.js
@@ -1,5 +1,39 @@
 let img, painters = [];
 
+const neilMotivationalQuotes = [
+    "You don’t need to be an expert!",
+    "Don’t worry if it’s a bit messy.",
+    "It’s great fun and anyone can do it.",
+    "Just be creative with it!",
+    "Give it a go!",
+    "Try it yourself!",
+    "See what you can come up with!",
+    "You can use whatever you’ve got lying around the house.",
+    "You don’t have to use exactly the same materials.",
+    "All you need is a bit of imagination.",
+    "There’s no right or wrong way to do it.",
+    "It doesn’t have to be perfect – it just has to be yours.",
+    "Art is all about expressing yourself.",
+    "You’d be surprised what you can do!",
+    "Have a go – you might just surprise yourself!",
+    "Use your imagination and have fun!",
+    "Mistakes are part of the fun.",
+    "Everyone sees art differently – that’s what makes it special.",
+    "The important thing is to enjoy yourself while doing it.",
+    "Don’t be afraid to try something new.",
+    "It’s not about getting it right – it’s about getting stuck in!",
+    "Every artist starts somewhere – why not start today?",
+    "If it looks different from mine, that’s a good thing!",
+    "Be bold, be brave, be creative!",
+    "Even a simple idea can turn into something amazing.",
+    "There’s no such thing as a silly idea in art.",
+    "You never know what you’re capable of until you try.",
+    "Add your own twist – make it yours!",
+    "The more you do, the better you’ll get.",
+    "Creativity is like a muscle – the more you use it, the stronger it gets!",
+    "Get stuck in and make something brilliant!"
+];
+
 class Painter {
     constructor(x,y) {
         this.x = x;
@@ -20,8 +54,17 @@ class Painter {
     }
 }
 
+function loadQuote() {
+    const randomQuote = neilMotivationalQuotes[Math.floor(Math.random() * neilMotivationalQuotes.length)];
+    const p = document.createElement("p");
+    p.innerHTML = `"${randomQuote}" <br> - <a href="https://en.wikipedia.org/wiki/Neil_Buchanan">Neil Buchanan</a>`;
+
+    const mainElement = document.querySelector("main");
+    mainElement.appendChild(p);
+}
+
 function preload() {
-    img = loadImage("./img/art_attack.png"); // Replace with your image file path or URL
+    img = loadImage("./img/art_attack.png");
   }
 
 function setup() {
@@ -30,6 +73,7 @@ function setup() {
     for(let i=0;i<500;i++) {
         painters.push(new Painter(floor(random(0,width)),floor(random(0,height))));
     }
+    loadQuote();
 }
 
 function draw() {
